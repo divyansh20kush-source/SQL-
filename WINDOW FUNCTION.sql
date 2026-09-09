6be43ba9a48b23441c766fe11eeab963a4abee56
Divyansh Kuswhaha <divyansh20kush@gmail.com>
2026-09-09 17:38:41 +0530

    Add files via upload

---
+++ WINDOW FUNCTION.sql
@@ -0,0 +1,21 @@
+-- Window Function
+
+select gender , avg(salary) as avg_salary
+from employee_demographics dem
+join employee_salary sal
+	on dem.employee_id= sal.employee_id
+group by gender;
+
+
+select dem.first_name , dem.last_name , gender , avg(salary) over(partition by gender)
+from employee_demographics dem
+join employee_salary sal
+	on dem.employee_id= sal.employee_id;
+    
+    
+select dem.first_name , dem.last_name , gender , gender , salary,
+ROW_NUMBER() OVER()
+from employee_demographics dem
+join employee_salary sal
+	on dem.employee_id= sal.employee_id;  
+    
